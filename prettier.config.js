9fa651321e95972add6cbcf1fd69cfc0d691dc4d
James Pizarro <jtgpizarro@gmail.com>
2022-01-10 10:13:39 +0800

    pushing latest release

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+const options = {
+    arrowParens: 'avoid',
+    singleQuote: true,
+    bracketSpacing: true,
+    endOfLine: 'lf',
+    semi: false,
+    tabWidth: 2,
+    TraillingComma: none,
+}
+module.exports = options
